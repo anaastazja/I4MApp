8cbebc71d3bc2f5536c8226703f81de82fa98400
anaastazja <anastazja.frontczak@gmail.com>
2024-07-03 12:05:09 +0200

    Add files via upload

---
+++ android/app/src/main/kotlin/com/example/industry4medical/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.industry4medical
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ android/app/src/main/kotlin/com/example/industry4medicalsmartphone/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.industry4medicalsmartphone
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
